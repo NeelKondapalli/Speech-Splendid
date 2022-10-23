aecad2f7c8afe5052ba4898737b917149a6346f0
Neelk1229 <neel2h06@gmail.com>
2022-10-23 00:39:56 -0700

    Changed skip_frame to allow for computation efficiency

--- utils/face.py
+++ utils/face.py
@@ -16,7 +16,7 @@ detector = load_detector()
 def analyze_face(tmp_file):
     cap = cv2.VideoCapture(tmp_file.name)
     frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    video_prediction = detector.detect_video(tmp_file.name, skip_frames = 100)
+    video_prediction = detector.detect_video(tmp_file.name, skip_frames = round(frames/3))
     emotions = ['fear', 'happiness', 'sadness', 'surprise', 'neutral', 'anger', 'disgust']
     readings = []
     for x in emotions:
@@ -39,4 +39,3 @@ def analyze_face(tmp_file):
     print(video_prediction.head())
     print(video_prediction.shape)
     print(sorted_emotion)
-    st.write("reached")
