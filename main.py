2c621705adbc522c2fd8181834a26f144bb2605e
Neelk1229 <neel2h06@gmail.com>
2022-10-23 01:11:10 -0700

    Fixed bug

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ inputCont = st.container()
 with inputCont:
     filename = st.file_uploader("Choose a recorded speech with your face clearly in frame", accept_multiple_files = False)
     st.write("\n\n")
-audio = st.checkbox('Audio Analysis')
-video = st.checkbox('Face Analysis')
+
 if filename is not None:
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file: 
         fp = Path(tmp_file.name)
         fp.write_bytes(filename.getvalue())
-    if audio:
-        st.write("## Speech Analysis Results")
-        speechCont = st.container()
-        with speechCont:
-            speech.extract_audio(tmp_file)
-    if video:
-        st.write("## Face Analysis Results")
-        faceCont = st.container()
-        with faceCont:
-            face.analyze_face(tmp_file)
+
+    st.write("## Speech Analysis Results")
+    speechCont = st.container()
+    with speechCont:
+        speech.extract_audio(tmp_file)
+
+    st.write("## Face Analysis Results")
+    faceCont = st.container()
+    with faceCont:
+        face.analyze_face(tmp_file)
