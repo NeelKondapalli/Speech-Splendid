50bf4fa5b9a47e91d463a577107bd2a3b810f4e3
Neelk1229 <neel2h06@gmail.com>
2022-10-23 01:02:31 -0700

    Added options

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ import utils.face as face
 st.write("# Speech-Splendid")
 inputCont = st.container()
 with inputCont:
-    filename = st.file_uploader("Choose a video file", accept_multiple_files = False)
+    filename = st.file_uploader("Choose a recorded speech with your face clearly in frame", accept_multiple_files = False)
     st.write("\n\n")
+speech = st.checkbox('Audio Analysis')
+face = st.checkbox('Face Analysis')
 if filename is not None:
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file: 
         fp = Path(tmp_file.name)
         fp.write_bytes(filename.getvalue())
-    
-    st.write("## Speech Analysis Results")
-    speechCont = st.container()
-    with speechCont:
-        speech.extract_audio(tmp_file)
-
-    st.write("## Face Analysis Results")
-    faceCont = st.container()
-    with faceCont:
-        face.analyze_face(tmp_file)
+    if speech:
+        st.write("## Speech Analysis Results")
+        speechCont = st.container()
+        with speechCont:
+            speech.extract_audio(tmp_file)
+    if face:
+        st.write("## Face Analysis Results")
+        faceCont = st.container()
+        with faceCont:
+            face.analyze_face(tmp_file)
